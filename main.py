0f6b557332c95290a3194b995df211c13d2180dc
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-09-08 15:06:13 -0700

    did some basic work / changed player attributes to be stored in dictionaries

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ import player
 import math
 
 players = []
+playerCount = 0
 
 def calculateMedals():
     pass
 
-for i in input("Number of players: "):
-    players.append(player.Player(input("Name: ")))
 
+while playerCount < 1 or playerCount > 6:
+    try:
+        playerCount = int(input("Number of players: "))
+        if not (playerCount >= 1 and playerCount <= 6):
+            print("Invalid number of players.") 
+    except ValueError:
+        print("Enter a valid number.")
+        playerCount = 0
+    
+
+
+for i in range(playerCount):
+    players.append(player.Player(input("Name: "), i))
+    players[i].stats["damage"] = input("How much damage has " + players[i].name + " dealt? ")
+    players[i].stats["eliminations"] = input("How many kills has " + players[i].name + " gotten? ")
+    players[i].stats["healing"] = input("How much healing has " + players[i].name + " given? ")
+    players[i].stats["objTime"] = input("How much objective time does " + players[i].name + " have? ")
+    players[i].stats["objKills"] = input("How many objective kills has " + players[i].name + " gotten? ")
+    players[i].stats["deaths"] = input("How many times has " + players[i].name + " died? ")
+    print(players[i].stats)
--- player.py
+++ player.py
@@ -2,26 +2,24 @@ class Player:
 
     # damage, elims, healing, deaths, objTime
 
-    def __init__(self, name):
+    def __init__(self, name, index):
         self.name = name
-        self.damage = 0
-        self.elims = 0
-        self.healing = 0
-        self.deaths = 0
-        self.objTime = 0
-        self.objkills = 0
-
-        self.damageMedal = None
-        self.elimMedal = None
-        self.healingMedal = None
-        self.deathsMedal = None
-        self.objTime = None
-        self.objkills = None
-
-
+        self.index = index
         
-        
+        self.stats = {
+            "damage": 0,
+            "eliminations": 0,
+            "healing": 0,
+            "objTime": 0,
+            "objKills": 0,
+            "deaths": 0
+        }
 
-    
-
-
+        self.medals = {
+            "damage": None,
+            "eliminations": None,
+            "healing": None,
+            "objTime": None,
+            "objKills": None,
+            "deaths": None
+        }

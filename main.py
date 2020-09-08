9a0246b6f2e3fb127943959b9698603cdb93ba3e
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-09-07 23:55:25 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import player
+import math
+
+players = []
+
+def calculateMedals():
+    pass
+
+for i in input("Number of players: "):
+    players.append(player.Player(input("Name: ")))
+
---
+++ player.py
@@ -0,0 +1,27 @@
+class Player:
+
+    # damage, elims, healing, deaths, objTime
+
+    def __init__(self, name):
+        self.name = name
+        self.damage = 0
+        self.elims = 0
+        self.healing = 0
+        self.deaths = 0
+        self.objTime = 0
+        self.objkills = 0
+
+        self.damageMedal = None
+        self.elimMedal = None
+        self.healingMedal = None
+        self.deathsMedal = None
+        self.objTime = None
+        self.objkills = None
+
+
+        
+        
+
+    
+
+

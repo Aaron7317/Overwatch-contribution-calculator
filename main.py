ad47c15c3530f782715b31f705cf47aeddd0f714
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-09-09 18:02:25 -0700

    made medal calculations

--- main.py
+++ main.py
@@ -4,8 +4,28 @@ import math
 players = []
 playerCount = 0
 
-def calculateMedals():
-    pass
+def confirmPositiveInt(message):
+    output = None
+    while type(output) != int or output < 0:
+        try:
+            output = int(input(message))
+        except:
+            print("Invalid value.")
+    return output
+        
+
+def calculateMedals(stat):
+    valueList = []
+    for i in players:
+        valueList.append(i.stats[stat])
+    valueList = sorted(valueList, reverse=True)
+    for i in players:
+        if i.stats[stat] == valueList[2]:
+            i.medals[stat] = "Bronze"
+        if i.stats[stat] == valueList[1]:
+            i.medals[stat] = "Silver"
+        if i.stats[stat] == valueList[0]:
+            i.medals[stat] = "Gold"
 
 
 while playerCount < 1 or playerCount > 6:
@@ -21,10 +41,20 @@ while playerCount < 1 or playerCount > 6:
 
 for i in range(playerCount):
     players.append(player.Player(input("Name: "), i))
-    players[i].stats["damage"] = input("How much damage has " + players[i].name + " dealt? ")
-    players[i].stats["eliminations"] = input("How many kills has " + players[i].name + " gotten? ")
-    players[i].stats["healing"] = input("How much healing has " + players[i].name + " given? ")
-    players[i].stats["objTime"] = input("How much objective time does " + players[i].name + " have? ")
-    players[i].stats["objKills"] = input("How many objective kills has " + players[i].name + " gotten? ")
-    players[i].stats["deaths"] = input("How many times has " + players[i].name + " died? ")
-    print(players[i].stats)
+    players[i].stats["damage"] = confirmPositiveInt("How much damage has " + players[i].name + " dealt? ")
+    players[i].stats["eliminations"] = confirmPositiveInt("How many kills has " + players[i].name + " gotten? ")
+    players[i].stats["healing"] = confirmPositiveInt("How much healing has " + players[i].name + " given? ")
+    players[i].stats["objTime"] = confirmPositiveInt("How much objective time does " + players[i].name + " have? ")
+    players[i].stats["objKills"] = confirmPositiveInt("How many objective kills has " + players[i].name + " gotten? ")
+    players[i].stats["deaths"] = confirmPositiveInt("How many times has " + players[i].name + " died? ")
+
+calculateMedals("damage")
+calculateMedals("eliminations")
+calculateMedals("healing")
+calculateMedals("objTime")
+calculateMedals("objKills")
+
+calculateMedals("deaths")
+
+for i in players:
+    print(i.medals)
